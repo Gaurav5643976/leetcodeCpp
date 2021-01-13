1b7359fa6826ef72f2f5642aab5963b67ca62e0c
Gaurav5643976 <mr.gaurav5643976@gmail.com>
2021-01-13 08:53:45 +0530

    1

---
+++ groupAnagrams49.cpp
@@ -0,0 +1,35 @@
+// Given an array of strings strs, group the anagrams together. You can return the answer in any order.
+
+// An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
+
+ 
+
+// Example 1:
+
+// Input: strs = ["eat","tea","tan","ate","nat","bat"]
+// Output: [["bat"],["nat","tan"],["ate","eat","tea"]]
+// Example 2:
+
+// Input: strs = [""]
+// Output: [[""]]
+// Example 3:
+
+// Input: strs = ["a"]
+// Output: [["a"]]
+
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<string,vector<string>> mp;
+        for(auto i:strs){
+            string a=i;
+            sort(a.begin(),a.end());
+            mp[a].push_back(i);
+        }
+        vector<vector<string>> v;
+        for(auto i:mp){
+            v.push_back(i.second);
+        }
+        return v;
+    }
+};
