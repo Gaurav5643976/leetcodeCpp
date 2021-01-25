9779eaf6e49fa88a7784dfe011f549748fcb74a1
Gaurav5643976 <mr.gaurav5643976@gmail.com>
2021-01-25 23:55:32 +0530

    25/1/21

---
+++ mergeKSortedLists23.cpp
@@ -0,0 +1,74 @@
+// You are given an array of k linked-lists lists, each linked-list is sorted in ascending order.
+
+// Merge all the linked-lists into one sorted linked-list and return it.
+
+ 
+
+// Example 1:
+
+// Input: lists = [[1,4,5],[1,3,4],[2,6]]
+// Output: [1,1,2,3,4,4,5,6]
+// Explanation: The linked-lists are:
+// [
+//   1->4->5,
+//   1->3->4,
+//   2->6
+// ]
+// merging them into one sorted list:
+// 1->1->2->3->4->4->5->6
+// Example 2:
+
+// Input: lists = []
+// Output: []
+// Example 3:
+
+// Input: lists = [[]]
+// Output: []
+ 
+
+// Constraints:
+
+// k == lists.length
+// 0 <= k <= 10^4
+
+// 0 <= lists[i].length <= 500
+// -10^4 <= lists[i][j] <= 10^4
+// lists[i] is sorted in ascending order.
+// The sum of lists[i].length won't exceed 10^4.
+
+
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* merge(ListNode* a, ListNode* b){
+        if(!a){
+            return b;
+        }
+        if(!b){
+            return a;
+        }
+        ListNode* head=(a->val<=b->val? a:b);
+        head->next=(a->val<=b->val?merge(a->next,b):merge(a,b->next));
+        return head;
+    }
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+        if(lists.size()==0){
+            return NULL;
+        }
+        ListNode* head=lists[0];
+        for(int i=1;i<lists.size();i++){
+            head=merge(head,lists[i]);
+        }
+        return head;
+    }
+};
