2dd88c3a162d034f3c5392c57485ab152760eab3
Gaurav5643976 <mr.gaurav5643976@gmail.com>
2021-03-09 12:42:50 +0530

    9/3/21

---
+++ numberOfGoodPairs1512.cpp
@@ -0,0 +1,50 @@
+// Given an array of integers nums.
+
+// A pair (i,j) is called good if nums[i] == nums[j] and i < j.
+
+// Return the number of good pairs.
+
+ 
+
+// Example 1:
+
+// Input: nums = [1,2,3,1,1,3]
+// Output: 4
+// Explanation: There are 4 good pairs (0,3), (0,4), (3,4), (2,5) 0-indexed.
+// Example 2:
+
+// Input: nums = [1,1,1,1]
+// Output: 6
+// Explanation: Each pair in the array are good.
+// Example 3:
+
+// Input: nums = [1,2,3]
+// Output: 0
+ 
+
+// Constraints:
+
+// 1 <= nums.length <= 100
+// 1 <= nums[i] <= 100
+
+
+class Solution {
+public:
+    int numIdenticalPairs(vector<int>& nums) {
+        unordered_map<int,int> m;
+        for(auto i:nums){
+            if(m.find(i)!=m.end()){
+                m[i]++;
+            }
+            else{
+                m[i]=1;
+            }
+        }
+        int ans=0;
+        for(auto i:nums){
+            m[i]--;
+            ans+=m[i];
+        }
+        return ans;
+    }
+};
