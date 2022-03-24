bbe588be6c03ad1f1cf6ac2001f37dfe3c39c1aa
Gaurav Singh <41682017+Gaurav5643976@users.noreply.github.com>
2022-03-24 12:24:31 +0530

    Time: 125 ms (25.12%), Space: 33.7 MB (32.30%) - LeetHub

--- 1654-minimum-jumps-to-reach-home/1654-minimum-jumps-to-reach-home.cpp
+++ 1654-minimum-jumps-to-reach-home/1654-minimum-jumps-to-reach-home.cpp
@@ -1,29 +1,28 @@
 class Solution {
 public:
     int minimumJumps(vector<int>& forbidden, int a, int b, int x) {
-        unordered_set<int> st;
+        unordered_set<int> st,vis;
         for(auto i:forbidden){
             st.insert(i);
         }
         queue<vector<int>> q;
         q.push({0,1,0});
+        vis.insert(0);
         while(!q.empty()){
             vector<int> v=q.front();
             q.pop();
             if(v[0]==x){
                 return v[2];
             }
-            if(st.find(v[0])!=st.end()){
-                continue;
-            }
-            st.insert(v[0]);
             int backward=v[0]-b;
-            if(v[1]!=0 and backward>=0){
+            if(v[1]!=0 and backward>=0 and st.find(backward)==st.end() and vis.find(backward)==vis.end()){
                 q.push({backward,0,v[2]+1});
+                vis.insert(backward);
             }
             int forward=v[0]+a;
-            if(v[0]<=2000+b){
+            if(forward<=10001 and st.find(forward)==st.end() and vis.find(forward)==vis.end()){
                 q.push({forward,1,v[2]+1});
+                vis.insert(forward);
             }
         }
         return -1;
