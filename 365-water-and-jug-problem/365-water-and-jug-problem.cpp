f0f4c04a3d215164debe69342a18e43c1c290f2e
Gaurav Singh <41682017+Gaurav5643976@users.noreply.github.com>
2022-03-24 13:10:46 +0530

    Time: 343 ms (25.84%), Space: 78.1 MB (17.01%) - LeetHub

--- 365-water-and-jug-problem/365-water-and-jug-problem.cpp
+++ 365-water-and-jug-problem/365-water-and-jug-problem.cpp
@@ -1,6 +1,24 @@
 class Solution {
 public:
     bool canMeasureWater(int x, int y, int z) {
-        return z==0 or (z<=x+y and z%__gcd(x,y)==0);
+        unordered_set<int> st;
+        queue<int> q;
+        q.push(0);
+        vector<int> dir={x,-x,y,-y};
+        while(!q.empty()){
+            int f=q.front();
+            q.pop();
+            if(f==z){
+                return true;
+            }
+            for(auto i:dir){
+                int val=i+f;
+                if(val>=0 and val<=x+y and st.find(val)==st.end()){
+                    st.insert(val);
+                    q.push(val);
+                }
+            }
+        }
+        return false;
     }
 };
