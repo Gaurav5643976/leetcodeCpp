7f25a05f7c92c0281ff828b66b5d2464c5928a1c
Gaurav Singh <41682017+Gaurav5643976@users.noreply.github.com>
2022-03-26 16:29:02 +0530

    Time: 1182 ms (5.02%), Space: 162.4 MB (5.23%) - LeetHub

---
+++ 752-open-the-lock/752-open-the-lock.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    set<string> generateNext(string str){
+        set<string> st;
+        string temp=str;
+        for(int i=0;i<4;i++){
+            char c=temp[i];
+            temp[i]=(c=='9'?'0':c+1);
+            st.insert(temp);
+            string b=str;
+            temp[i]=(c=='0'?'9':c-1);
+            st.insert(temp);
+            temp[i]=c;
+        }
+        return st;
+    }
+    int openLock(vector<string>& deadends, string target) {
+        unordered_set<string> deadSet(deadends.begin(),deadends.end());
+        string start="0000";
+        if(deadSet.count(start) or deadSet.count(target)){
+            return -1;
+        }
+        queue<pair<string,int>> q;
+        q.push({start,0});
+        deadSet.insert(start);
+        while(!q.empty()){
+            string cur=q.front().first;
+            int turns=q.front().second;
+            q.pop();
+            if(cur==target){
+                return turns;
+            }
+            for(auto i:generateNext(cur)){
+                if(!deadSet.count(i)){
+                    q.push({i,turns+1});
+                    deadSet.insert(i);
+                }
+            }
+        }
+        return -1;
+    }
+};
