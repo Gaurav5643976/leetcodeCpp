d31c425a24378a002c4ca57cbdcbab4811bfee6c
Gaurav Singh <41682017+Gaurav5643976@users.noreply.github.com>
2022-03-29 21:48:12 +0530

    Time: 1171 ms (5.05%), Space: 91.6 MB (7.02%) - LeetHub

---
+++ 1631-path-with-minimum-effort/1631-path-with-minimum-effort.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int minimumEffortPath(vector<vector<int>>& heights) {
+        int m=heights.size(),n=heights[0].size();
+        using vi=vector<int>;
+        vector<vi> dist(m,vi(n,INT_MAX));
+        priority_queue<vi,vector<vi>, greater<vi>> pq;
+        pq.push({0,0,0});
+        dist[0][0]=0;
+        vector<vi> dir={{0,1},{1,0},{-1,0},{0,-1}};
+        while(!pq.empty()){
+            vi v=pq.top();
+            pq.pop();
+            for(auto i:dir){
+                int x=v[1]+i[0],y=v[2]+i[1];
+                if(x>=0 and x<m and y>=0 and y<n){
+                    int tempDist=max(v[0],abs(heights[x][y]-heights[v[1]][v[2]]));
+                    if(dist[x][y]>tempDist){
+                        dist[x][y]=tempDist;
+                        pq.push({tempDist,x,y});
+                    }
+                }
+            }
+        }
+        return dist[m-1][n-1];
+    }
+};
