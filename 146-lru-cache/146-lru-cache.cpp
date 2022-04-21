713a7780232b36fc04d64970abb926cda9546483
Gaurav Singh <41682017+Gaurav5643976@users.noreply.github.com>
2022-04-21 10:55:56 +0530

    Time: 668 ms (34.39%), Space: 174.1 MB (43.54%) - LeetHub

---
+++ 146-lru-cache/146-lru-cache.cpp
@@ -0,0 +1,45 @@
+class LRUCache {
+private:
+    //typedef list<int> LI;
+    //typedef pair<int, list<int>::iterator> PII;
+    //typedef unordered_map<int, pair<int, list<int>::iterator>> HIPII;
+    
+    void touch(unordered_map<int, pair<int, list<int>::iterator>>::iterator it) {
+        int key = it->first;
+        used.erase(it->second.second);
+        used.push_front(key);
+        it->second.second = used.begin();
+    }
+    
+    unordered_map<int, pair<int, list<int>::iterator>> cache;
+    list<int> used;
+    int _capacity;
+public:
+    LRUCache(int capacity){
+        _capacity=capacity;
+    }
+    
+    int get(int key) {
+        auto it = cache.find(key);
+        if (it == cache.end()){
+            return -1;
+        } 
+        touch(it);
+        return it->second.first;
+    }
+    
+    void put(int key, int value) {
+        auto it = cache.find(key);
+        if (it != cache.end()){
+             touch(it);
+        }
+        else {
+			if (cache.size() == _capacity) {
+				cache.erase(used.back());
+				used.pop_back();
+			}
+            used.push_front(key);
+        }
+        cache[key] = { value, used.begin() };
+    }
+};
