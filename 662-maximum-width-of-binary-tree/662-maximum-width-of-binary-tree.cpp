2673e15c1d9efb587f0e14d21a66c19f1008d7b4
Gaurav Singh <41682017+Gaurav5643976@users.noreply.github.com>
2022-04-28 10:52:28 +0530

    Time: 14 ms (43.34%), Space: 17.2 MB (97.72%) - LeetHub

--- 662-maximum-width-of-binary-tree/662-maximum-width-of-binary-tree.cpp
+++ 662-maximum-width-of-binary-tree/662-maximum-width-of-binary-tree.cpp
@@ -12,30 +12,34 @@
 class Solution {
 public:
     int widthOfBinaryTree(TreeNode* root) {
-        if(!root)
+        if(root==NULL){
             return 0;
-        int ans = 0;
-        queue<pair<TreeNode*, long long>> q;
+        }
+        int ans=0;
+        queue<pair<TreeNode*,unsigned long long>> q;
         q.push({root,0});
         while(!q.empty()){
-            int size = q.size();
-            long long mmin = q.front().second;    //to make the id starting from zero
-            long long first,last;
-            for(int i=0; i<size; i++){
-                long long cur_id = q.front().second-mmin;
-                TreeNode* node = q.front().first;
+            int sz=q.size();
+            unsigned long long v=q.front().second;
+            unsigned long long last=0,first=0;
+            for(int i=0;i<sz;i++){
+                pair<TreeNode*,unsigned long long> p=q.front();
                 q.pop();
-                if(i==0)
-                    first = cur_id;
-                if(i==size-1)
-                    last = cur_id;
-                if(node->left)
-                    q.push({node->left, cur_id*2});
-                if(node->right)
-                    q.push({node->right, cur_id*2+1});
+                if(i==0){
+                    first=p.second;
+                }
+                if(i==sz-1){
+                    last=p.second;
+                }
+                if(p.first->left){
+                    q.push({p.first->left,2*(p.second-v)});
+                }
+                if(p.first->right){
+                    q.push({p.first->right,2*(p.second-v)+1});
+                }
             }
-            int a=last-first+1;
-            ans = max(ans, a);
+            int res=last-first+1;
+            ans=max(ans,res);
         }
         return ans;
     }
